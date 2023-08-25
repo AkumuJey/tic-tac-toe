be0c3e4a69012d3fcfc955b85e752378021ab535
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-08-25 14:21:40 +0300

    Added square components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,26 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React from "react";
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square className="square"/>
+        <Square className="square"/>
+        <Square className="square"/>
+      </div>
+      <div className="board-row">
+        <Square className="square"/>
+        <Square className="square"/>
+        <Square className="square"/>
+      </div>
+      <div className="board-row">
+        <Square className="square"/>
+        <Square className="square"/>
+        <Square className="square"/>
+      </div>
+    </div>
+  );
+}
+
+function Square () {
+  return <button>1</button>
 }
